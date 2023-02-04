1c3d446770f2981e8cc4490198e4a947e260567a
篠原 琉雅 <raitan3170@gmail.com>
2023-02-05 06:12:08 +0900

    暗号化・復号化が出来るようになった

--- Assets/Scripts/JsonProcess.cs
+++ Assets/Scripts/JsonProcess.cs
@@ -1,8 +1,10 @@
-using System;
-using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 public class JsonProcess : MonoBehaviour
 {
@@ -10,7 +12,7 @@ public class JsonProcess : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Save();
+            CreateJsonFile();
         }
 
         if (Input.GetKeyDown(KeyCode.L))
@@ -23,25 +25,111 @@ public class JsonProcess : MonoBehaviour
     }
 
     /// <summary>データを保存 </summary>
-    void Save()
+    void Save(bool flag)
     {
         Debug.Log("保存");
-        var path = Application.persistentDataPath + "/PlayData/" + "PlayData.json";
+        var path = Application.persistentDataPath + "PlayData.json";
         var data = new Data(10, 10, 10);
         var json = JsonUtility.ToJson(data);
-        File.WriteAllText(path, json);
+
+
+        if (flag)
+        {
+            var jsonByte = Encoding.UTF8.GetBytes(json);
+            jsonByte = AesEncrypt(jsonByte);
+
+            File.WriteAllBytes(path, jsonByte);
+        }
+        else
+        {
+            File.WriteAllText(path, json);
+        }
+    }
+
+    /// <summary>データを保存する </summary>
+    public void CreateJsonFile()
+    {
+        Debug.Log("保存");
+        var savePath = Application.persistentDataPath + "PlayData.json";     //保存先のパス
+        var saveData = JsonUtility.ToJson(new Data(30, 30, 30));                  //保存するデータを作成
+        File.WriteAllText(savePath, saveData);  //データを書き込む
     }
 
     /// <summary>データを読み込み</summary>
     Data Load()
     {
         Debug.Log("読み込み");
-        var path = Application.dataPath + "/PlayData/" + "PlayData.json";
-        var json = File.ReadAllText(path);
-        var data = JsonUtility.FromJson<Data>(json);
+        var path = Application.persistentDataPath + "PlayData.json";
+        var byteData = File.ReadAllBytes(path);
+        byteData = AesDecrypt(byteData);
 
-        return new Data(10, 10, 10);
+        var json = Encoding.UTF8.GetString(byteData);
+        // var json = File.ReadAllText(path);
+       
+        return JsonUtility.FromJson<Data>(json);
     }
+
+    /// <summary>
+    /// AES暗号化
+    /// </summary>
+    byte[] AesEncrypt(byte[] byteText)
+    {
+        // AES設定値　aesIv・aesKeyは暗号化と複合化で同じものを使用する
+        //===================================
+        int aesKeySize = 128;
+        int aesBlockSize = 128;
+        string aesIv = "6KGhH66PeU3cSLS7";
+        string aesKey = "R38FYEzPyjxv0HrE";
+        //===================================
+
+        // AESマネージャー取得
+        var aes = GetAesManager(aesKeySize, aesBlockSize, aesIv, aesKey);
+        // 暗号化
+        byte[] encryptText = aes.CreateEncryptor().TransformFinalBlock(byteText, 0, byteText.Length);
+
+        return encryptText;
+    }
+
+    /// <summary>
+    /// AES復号化
+    /// </summary>
+    byte[] AesDecrypt(byte[] byteText)
+    {
+        // AES設定値
+        //===================================
+        int aesKeySize = 128;
+        int aesBlockSize = 128;
+        string aesIv = "6KGhH66PeU3cSLS7";
+        string aesKey = "R38FYEzPyjxv0HrE";
+        //===================================
+
+        // AESマネージャー取得
+        var aes = GetAesManager(aesKeySize, aesBlockSize, aesIv, aesKey);
+        // 復号化
+        byte[] decryptText = aes.CreateDecryptor().TransformFinalBlock(byteText, 0, byteText.Length);
+
+        return decryptText;
+    }
+
+    /// <summary>
+    /// AesManagedを取得
+    /// </summary>
+    /// <param name="keySize">暗号化鍵の長さ</param>
+    /// <param name="blockSize">ブロックサイズ</param>
+    /// <param name="iv">初期化ベクトル(半角X文字（8bit * X = [keySize]bit))</param>
+    /// <param name="key">暗号化鍵 (半X文字（8bit * X文字 = [keySize]bit）)</param>
+    AesManaged GetAesManager(int keySize, int blockSize, string iv, string key)
+    {
+        AesManaged aes = new AesManaged();
+        aes.KeySize = keySize;
+        aes.BlockSize = blockSize;
+        aes.Mode = CipherMode.CBC;
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.Padding = PaddingMode.PKCS7;
+        return aes;
+    }
+
 }
 
 [SerializeField]
